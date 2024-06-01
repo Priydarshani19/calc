9bc07b6598a4d379eb259ce167239e4b370fdc41
Priydarshani19 <165897473+Priydarshani19@users.noreply.github.com>
2024-06-01 15:57:40 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+ope=input("enter operator:")
+if(ope=='+'):
+    print("Addition=>",num1+num2)
+elif(ope=='-'):
+    print("Substarction=>",num1-num2)
+elif(ope=='*'):
+    print("Multiplication=>",num1*num2)
+elif(ope=='/'):
+    print("Division=>",num1/num2)
+elif(ope=='%'):
+    print("Modulo=>",num1%num2)
